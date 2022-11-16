9bb0b604652ddeae2b388db9c2575b3aec149c12
abdulbasit01 <abdulbasit14827@gmail.com>
2022-11-17 01:29:34 +0500

    upates

--- listng.js
+++ listng.js
@@ -9,45 +9,69 @@ function loaddata() {
 
 function insertDataIntoTable(contacts) {
   const tableBody = document.getElementById("table-body");
-  contacts.forEach((element, index) => {
+  contacts.forEach((element) => {
     const parentRow = document.createElement("tr");
-    const nameColPhone = document.createElement("td");
-    nameColPhone.innerHTML = `${element.FName} ${element.LName}`
+
+    const parentColName = document.createElement("td");
+    parentColName.innerHTML = `${element.fname} ${element.lname}`;
+    parentColName.setAttribute(
+      "onclick",
+      "changeLoc(" + element.contactId + ",'details.html')"
+    );
     const parentColAction = document.createElement("td");
     const deleteBtn = document.createElement("i");
     const editBtn = document.createElement("i");
-    const details = document.createElement("i");
-    deleteBtn.setAttribute("class", "fa-solid fa-trash-can text-dark mx-2 btn btn-danger btn-sm");
+    const detailsBtn = document.createElement("i");
+    deleteBtn.setAttribute(
+      "class",
+      "fa-solid fa-trash-can text-dark mx-2 btn btn-danger btn-sm"
+    );
     deleteBtn.setAttribute("onclick", "toggleModal(" + element.contactId + ")");
+    deleteBtn.setAttribute("title", "Click to delete this contact");
     parentColAction.appendChild(deleteBtn);
-    editBtn.setAttribute("class", "fa-solid mx-2 btn btn-secondary btn-sm fa-pen-to-square");
-    editBtn.setAttribute("onclick", "editFn(" + element.contactId + ")");
+    detailsBtn.setAttribute(
+      "class",
+      "fa-solid fa-user text-dark mx-2 btn btn-success btn-sm"
+    );
+    detailsBtn.setAttribute("title", "Click to view details");
+    detailsBtn.setAttribute(
+      "onclick",
+      "changeLoc(" + element.contactId + ",'details.html')"
+    );
+    parentColAction.appendChild(detailsBtn);
+    editBtn.setAttribute(
+      "class",
+      "fa-solid mx-2 btn btn-secondary btn-sm fa-pen-to-square"
+    );
+    editBtn.setAttribute(
+      "onclick",
+      "changeLoc(" + element.contactId + ",'editcontat.html')"
+    );
+    editBtn.setAttribute("title", "Click to update this contact");
     parentColAction.appendChild(editBtn);
-    details.setAttribute("class", "fa-solid fa-circle-info text-dark mx-2 btn btn-primary btn-sm")
-    details.setAttribute("onclick", "detailsFn(" + element.contactId + ")")
-    parentColAction.appendChild(details)
-    parentRow.appendChild(nameColPhone);
+    parentRow.appendChild(parentColName);
     parentRow.appendChild(parentColAction);
     parentRow.setAttribute("class", "border m-1 text-center");
     tableBody.appendChild(parentRow);
   });
   $("#contact-tables")
     .DataTable({
-      pagingType: 'simple',
+      pagingType: "simple",
       responsive: true,
       language: {
         searchPlaceholder: "Search Contact.",
         search: "",
-        'paginate': {
-          'previous': '<span class="prev-icon"><i class="fa-solid fa-arrow-left"></i></span>',
-          'next': '<span class="next-icon"><i class="fa-solid fa-arrow-right"></i></span>'
-        }
+        paginate: {
+          previous:
+            '<span class="prev-icon"><i class="fa-solid fa-arrow-left"></i></span>',
+          next: '<span class="next-icon"><i class="fa-solid fa-arrow-right"></i></span>',
+        },
       },
       pageLength: 5,
       lengthMenu: [
         [5, 10, 15, 20, -1],
-        [5, 10, 15, 20, "All"]
-      ]
+        [5, 10, 15, 20, "All"],
+      ],
     })
     .columns.adjust();
 }
@@ -56,30 +80,22 @@ function toggleModal(id) {
   const actionmodal = document.getElementById("actionmodal");
   actionmodal.setAttribute("delete-action-id", id);
   actionmodal.classList.toggle("d-none");
-  document.getElementsByTagName('body')[0].classList.toggle("overflow-hidden")
-  document.getElementById('main-c-table').classList.toggle("blur")
+  document.getElementsByTagName("body")[0].classList.toggle("overflow-hidden");
+  document.getElementById("main-c-table").classList.toggle("blur");
 }
-function editFn(data) {
-  window.location = "editcontat.html#" + data;
+function changeLoc(data, name) {
+  window.location = `${name}#` + data;
 }
 function deleteFn() {
   const actionmodal = document.getElementById("actionmodal");
   const filterData = JSON.parse(
     localStorage.getItem("contact-information")
   ).filter((data) => {
+    console.log(data.contactId, actionmodal.getAttribute("delete-action-id"));
     if (data.contactId != +actionmodal.getAttribute("delete-action-id")) {
       return data;
     }
   });
   localStorage.setItem("contact-information", JSON.stringify(filterData));
-  window.location.reload();
+  window.location = "listing.html";
 }
-function detailsFn(data) {
-  window.location = "details.html#" + data;
-}
-
-document.getElementById("Edit-btn").addEventListener('click', (event) => {
-  editFn(window.location.hash.substring(1))
-})
-document.getElementById("Delete-btn").addEventListener('click', (event) => {
-})
--- script.js
+++ script.js
@@ -1,62 +1,69 @@
 function loadupdateformdata() {
-  preLoader()
+  preLoader();
   const queryParam = window.location.hash;
   const data = JSON.parse(localStorage.getItem("contact-information"));
   for (let index = 0; index < data.length; index++) {
     if (data[index].contactId == queryParam.substring(1)) {
       document.getElementById("phonenumber").defaultValue =
         data[index].phonenumber;
+      document.getElementById("lname").defaultValue = data[index].lname;
+      document.getElementById("fname").defaultValue = data[index].fname;
       document.getElementById("emailaddress").defaultValue =
         data[index].emailaddress;
-      document.getElementById("FName").defaultValue =
-        data[index].FName;
-      document.getElementById("LName").defaultValue =
-        data[index].LName;
+      document.getElementById("contactId").defaultValue = data[index].contactId;
     }
   }
 }
 function loaddata() {
-  preLoader()
+  preLoader();
   if (!localStorage.getItem("contact-information")) return [];
   return JSON.parse(localStorage.getItem("contact-information"));
 }
-function loadParticularContactDetails(params) {
-  preLoader()
+function loaddetailsdata() {
+  preLoader();
   const queryParam = window.location.hash;
   const data = JSON.parse(localStorage.getItem("contact-information"));
-}
-function addContact(val) {
-  const formData = new FormData(val);
-  let jsonContact = {};
-  for (const [key, value] of formData.entries()) {
-    if (!value.trim()) {
-      errorBinding(key);
-    } else {
-      errorClearing(key);
-      jsonContact = {
-        ...jsonContact,
-        [key]: value,
-        contactId: loaddata().length + 1,
-      };
+  for (let index = 0; index < data.length; index++) {
+    if (data[index].contactId == queryParam.substring(1)) {
+      document.getElementById("fname").innerHTML = data[index].fname;
+
+      document.getElementById("lname").innerHTML = data[index].lname;
+
+      document.getElementById(
+        "email"
+      ).innerHTML = `<a href=mailto:${data[index].emailaddress} class="text-dark">${data[index].emailaddress}</a>`;
+      document.getElementById(
+        "contact"
+      ).innerHTML = `<a href=tel:${data[index].phonenumber} class="text-dark">${data[index].phonenumber}</a>`;
+      const dtpagedbtn = document.getElementById("details-page-delete-btn");
+      dtpagedbtn.setAttribute("deletebtn-id", data[index].contactId);
     }
   }
-  if (Object.keys(jsonContact).length > 2) {
-    localStorage.setItem(
-      "contact-information",
-      JSON.stringify([...loaddata(), jsonContact])
-    );
-    val.reset();
-    showNotification(val)
-  }
+}
+function addContact(val) {
+  const jsonContact = {
+    fname: document.getElementById("fname").value,
+    lname: document.getElementById("lname").value,
+    phonenumber: document.getElementById("phonenumber").value,
+    emailaddress: document.getElementById("emailaddress").value,
+    contactId: loaddata().length + 1,
+  };
+  const errorIds = {
+    fname: "fname-error",
+    lname: "lname-error",
+    phonenumber: "phone-error",
+    emailaddress: "email-error",
+  };
+  errorChecking(jsonContact, errorIds);
+  localStorage.setItem(
+    "contact-information",
+    JSON.stringify([...loaddata(), jsonContact])
+  );
+  showNotification(val);
 }
 
-function errorBinding(key) {
-  document.getElementById(key).classList.remove("d-none");
-  return;
-}
-function errorClearing(key) {
-  document.getElementById(key).classList.add("d-none");
-  return;
+function errorChecking(values, keys) {
+  
 }
 function updateContact(val) {
   const formData = new FormData(val);
@@ -75,17 +82,35 @@ function updateContact(val) {
     }
   }
   localStorage.setItem("contact-information", JSON.stringify(tempData));
-  showNotification(val)
+  showNotification(val);
 }
 
 function showNotification(val) {
-  $("#notificaion-bar").show().animate({
-    opacity: 1,
-  }, 4000, function () {
-    $(this).hide("slow");
-  });
+  $("#notificaion-bar")
+    .show()
+    .animate(
+      {
+        opacity: 1,
+      },
+      4000,
+      function () {
+        $(this).hide("slow");
+      }
+    );
 }
 function preLoader() {
   $("#status").fadeOut();
   $("#preloader").delay(500).fadeOut("slow");
+}
+
+function deleteFnOfDetPage() {
+  const deletebtnID = document
+    .getElementById("details-page-delete-btn")
+    .getAttribute("deletebtn-id");
+
+  const actionmodal = document.getElementById("actionmodal");
+  actionmodal.setAttribute("delete-action-id", deletebtnID);
+  actionmodal.classList.toggle("d-none");
+  document.getElementsByTagName("body")[0].classList.toggle("overflow-hidden");
+  document.getElementById("main-c-table").classList.toggle("blur");
 }
